f4738a5c7b05cb8b0f008fe39a371f2bf8887bea
wuwl <wuwl@mail.jj.cn>
2024-01-31 11:45:55 +0800

    The most significant change is the addition of a new function called `distinctDifferenceArray` to the `Solution` class in the `N2670` namespace. This function calculates the difference in the number of distinct elements in the prefix and suffix of an input vector at each index.
    Changes:
    - A new function `distinctDifferenceArray` has been added to the `Solution` class in the `N2670` namespace. This function takes a vector of integers as input and returns a vector of integers. The function calculates the difference in the number of distinct elements in the prefix and suffix of the input vector at each index and stores these differences in a result vector. The function uses two unordered maps to keep track of the counts of elements in the prefix and suffix. The function iterates over the input vector, updating the counts in the maps and the result vector at each step. The function finally returns the result vector.

--- source.cpp
+++ source.cpp
@@ -778,6 +778,41 @@ public:
 };
 }
 
+// 2670. 找出不同元素数目差数组
+namespace N2670
+{
+//给你一个下标从 0 开始的数组 nums ，数组长度为 n 。
+//
+//nums 的 不同元素数目差 数组可以用一个长度为 n 的数组 diff 表示，其中 diff[i] 等于前缀 nums[0, ..., i] 中不同元素的数目 减去 后缀 nums[i + 1, ..., n - 1] 中不同元素的数目。
+//
+//返回 nums 的 不同元素数目差 数组。
+//
+//注意 nums[i, ..., j] 表示 nums 的一个从下标 i 开始到下标 j 结束的子数组（包含下标 i 和 j 对应元素）。特别需要说明的是，如果 i > j ，则 nums[i, ..., j] 表示一个空子数组。
+class Solution {
+public:
+  vector<int> distinctDifferenceArray(vector<int>& nums) {
+    vector<int> res(nums.size(), 0);
+    unordered_map<int, int> pre;
+    unordered_map<int, int> suf;
+    for (size_t i = 0; i < nums.size(); ++i)
+    {
+      suf[nums[i]]++;
+    }
+    for (size_t i = 0; i < nums.size(); i++)
+    {
+      pre[nums[i]]++;
+      suf[nums[i]]--;
+      if (suf[nums[i]] == 0)
+      {
+        suf.erase(nums[i]);
+      }
+      res[i] = static_cast<int>(pre.size()) - static_cast<int>(suf.size());
+    }
+    return res;
+  }
+};
+}
+
 // 2707. 字符串中的额外字符
 namespace N2707
 {
