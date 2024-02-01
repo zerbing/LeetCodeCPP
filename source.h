993d409909851d4c07731081c6ced9163d9a2095
wuwl <wuwl@mail.jj.cn>
2024-02-01 15:43:05 +0800

    1. 100. 相同的树 2. 106. 从中序与后序遍历序列构造二叉树

--- source.h
+++ source.h
@@ -462,7 +462,6 @@ public:
 namespace N99
 {
 // 给你二叉搜索树的根节点 root ，该树中的 恰好 两个节点的值被错误地交换。请在不改变其结构的情况下，恢复这棵树
-// Definition for a binary tree node.
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -533,6 +532,81 @@ public:
     traverse(root);
     findNodes();
     swapNodes(root);
+  }
+};
+}
+
+// 100. 相同的树
+namespace N100
+{
+//给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+//
+//如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+  bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (p == nullptr && q == nullptr)
+    {
+      return true;
+    }
+    if (p == nullptr || q == nullptr)
+    {
+      return false;
+    }
+    if (p->val != q->val)
+    {
+      return false;
+    }
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+  }
+};
+}
+
+// 106. 从中序与后序遍历序列构造二叉树
+namespace N106
+{
+//给定两个整数数组 inorder 和 postorder ，其中 inorder 是二叉树的中序遍历， postorder 是同一棵树的后序遍历，请你构造并返回这颗 二叉树。
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+  TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+    if (inorder.empty() || postorder.empty())
+    {
+      return nullptr;
+    }
+    int root = *postorder.rbegin();
+    int inorderRootIndex = 0;
+    for (int i = 0; i < inorder.size(); ++i)
+    {
+      if (inorder[i] == root)
+      {
+        inorderRootIndex = i;
+        break;
+      }
+    }
+    TreeNode* node = new TreeNode(root);
+    vector<int> leftInorder(inorder.begin(), inorder.begin() + inorderRootIndex);
+    vector<int> rightInorder(inorder.begin() + inorderRootIndex + 1, inorder.end());
+    vector<int> leftPostorder(postorder.begin(), postorder.begin() + inorderRootIndex);
+    vector<int> rightPostorder(postorder.begin() + inorderRootIndex, postorder.end() - 1);
+    node->left = buildTree(leftInorder, leftPostorder);
+    node->right = buildTree(rightInorder, rightPostorder);
+    return node;
   }
 };
 }
