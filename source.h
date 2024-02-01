2c458f2e99ecca273b113d8819cae7176d6bc1bb
wuwl <wuwl@mail.jj.cn>
2024-02-01 17:25:01 +0800

    1. 129. 求根到叶子节点数字之和

--- source.h
+++ source.h
@@ -1007,6 +1007,50 @@ public:
 };
 }
 
+// 129. 求根到叶子节点数字之和
+namespace N129
+{
+//给你一个二叉树的根节点 root ，树中每个节点都存放有一个 0 到 9 之间的数字。
+//每条从根节点到叶节点的路径都代表一个数字：
+//
+//例如，从根节点到叶节点的路径 1 -> 2 -> 3 表示数字 123 。
+//计算从根节点到叶节点生成的 所有数字之和 。
+//
+//叶节点 是指没有子节点的节点。
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+  int sumNumbers(TreeNode* root) {
+    int res = 0;
+    function<void(TreeNode* root, int sum)> traverse = [&](TreeNode* root, int sum)
+      {
+        if (nullptr == root)
+        {
+          return;
+        }
+        sum = sum * 10 + root->val;
+        if (nullptr == root->left && nullptr == root->right)
+        {
+          res += sum;
+        }
+        traverse(root->left, sum);
+        traverse(root->right, sum);
+      };
+    traverse(root, 0);
+    return res;
+  }
+};
+}
+
 // 383. 赎金信
 namespace N383
 {
