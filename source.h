a29dc4eabbc776566b539377aec40b327cc475be
wuwl <wuwl@mail.jj.cn>
2024-02-01 16:57:46 +0800

    1. 116. 填充每个节点的下一个右侧节点指针

--- source.h
+++ source.h
@@ -841,6 +841,70 @@ public:
 };
 }
 
+// 116. 填充每个节点的下一个右侧节点指针
+namespace N116
+{
+//给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+//
+//struct Node {
+//  int val;
+//  Node* left;
+//  Node* right;
+//  Node* next;
+//}
+//填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+//
+//初始状态下，所有 next 指针都被设置为 NULL。
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+class Solution {
+public:
+  Node* connect(Node* root) {
+    queue<Node*> q;
+    if (root != nullptr)
+    {
+      q.push(root);
+    }
+    while (!q.empty())
+    {
+      size_t size = q.size();
+      for (size_t i = 0; i < size; ++i)
+      {
+        Node* node = q.front();
+        q.pop();
+        if (i < size - 1)
+        {
+          node->next = q.front();
+        }
+        if (node->left != nullptr)
+        {
+          q.push(node->left);
+        }
+        if (node->right != nullptr)
+        {
+          q.push(node->right);
+        }
+      }
+    }
+    return root;
+  }
+};
+}
+
 // 117. 填充每个节点的下一个右侧节点指针 II
 namespace N117
 {
