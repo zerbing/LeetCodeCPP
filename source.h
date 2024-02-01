abe26040664d3e40c591a8323128174e0af3d0d2
wuwl <wuwl@mail.jj.cn>
2024-02-01 17:28:34 +0800

    1. 144. 二叉树的前序遍历

--- source.h
+++ source.h
@@ -1051,6 +1051,42 @@ public:
 };
 }
 
+// 144. 二叉树的前序遍历
+namespace N144
+{
+// 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+  vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> res;
+    stack<TreeNode*> st;
+    TreeNode* cur = root;
+    while (cur != nullptr || !st.empty())
+    {
+      while (cur != nullptr)
+      {
+        res.push_back(cur->val);
+        st.push(cur);
+        cur = cur->left;
+      }
+      cur = st.top();
+      st.pop();
+      cur = cur->right;
+    }
+    return res;
+  }
+};
+}
+
 // 383. 赎金信
 namespace N383
 {
