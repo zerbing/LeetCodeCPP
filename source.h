fc73198b833e1d15dd8cabcaabe6d0448effac06
wuwl <wuwl@mail.jj.cn>
2024-02-01 16:32:46 +0800

    1. 112. 路径总和

--- source.h
+++ source.h
@@ -762,6 +762,44 @@ public:
 };
 }
 
+// 112. 路径总和
+namespace N112
+{
+//给你二叉树的根节点 root 和一个表示目标和的整数 targetSum 。判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。如果存在，返回 true ；否则，返回 false 。
+//
+//叶子节点 是指没有子节点的节点。
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+  bool hasPathSum(TreeNode* root, int targetSum) {
+    int res = false;
+    function<void(TreeNode* root, int)> dfs = [&](TreeNode* root, int sum)
+      {
+        if (nullptr == root || true == res)
+        {
+          return;
+        }
+        if (nullptr == root->left && nullptr == root->right && sum + root->val == targetSum)
+        {
+          res = true;
+        }
+        dfs(root->left, sum + root->val);
+        dfs(root->right, sum + root->val);
+      };
+    dfs(root, 0);
+    return res;
+  }
+};
+}
+
 // 117. 填充每个节点的下一个右侧节点指针 II
 namespace N117
 {
