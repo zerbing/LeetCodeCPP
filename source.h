c3101494a0cc03ea6398e1f4b6611cc72486cd63
wuwl <wuwl@mail.jj.cn>
2024-02-01 16:08:05 +0800

    1. 110. 平衡二叉树

--- source.h
+++ source.h
@@ -668,6 +668,46 @@ public:
 };
 }
 
+// 110. 平衡二叉树
+namespace N110
+{
+//给定一个二叉树，判断它是否是高度平衡的二叉树。
+//
+//本题中，一棵高度平衡二叉树定义为：
+// 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+  bool isBalanced(TreeNode* root) {
+    bool res = true;
+    function<int(TreeNode*)> traverse = [&](TreeNode* node)->int
+      {
+        if (node == nullptr || res == false)
+        {
+          return 0;
+        }
+        int nLeft = traverse(node->left);
+        int nRight = traverse(node->right);
+        if (abs(nLeft - nRight) > 1)
+        {
+          res = false;
+        }
+        return max(nLeft, nRight) + 1;
+      };
+    traverse(root);
+    return res;
+  }
+};
+}
+
 // 117. 填充每个节点的下一个右侧节点指针 II
 namespace N117
 {
