6fa24c7aeaa80c545fe377ff95e7f75edb03165d
wuwl <wuwl@mail.jj.cn>
2024-02-02 17:24:09 +0800

    120. 三角形最小路径和 (中等)

--- dfs.h
+++ dfs.h
@@ -44,3 +44,44 @@ public:
   }
 };
 }
+
+// 120. 三角形最小路径和 (中等)
+namespace N120
+{
+//给定一个三角形 triangle ，找出自顶向下的最小路径和。
+//
+//每一步只能移动到下一行中相邻的结点上。相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。也就是说，如果正位于当前行的下标 i ，那么下一步可以移动到下一行的下标 i 或 i + 1 。
+//例如，给定三角形：
+//   2
+//  3 4
+// 6 5 7
+//4 1 8 3
+//自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
+class Solution {
+public:
+  int minimumTotal(vector<vector<int>>& triangle) {
+    vector<vector<int>> memo;
+    for (size_t i = 0; i < triangle.size(); ++i)
+    {
+      memo.push_back(vector<int>(triangle[i].size(), INT_MAX));
+    }
+    int res = INT_MAX;
+    function<void(int, int, int)> dfs = [&](int i, int j, int sum) {
+      if (i == triangle.size())
+      {
+        res = min(res, sum);
+        return;
+      }
+      if (memo[i][j] <= sum)
+      {
+        return;
+      }
+      memo[i][j] = sum;
+      dfs(i + 1, j, sum + triangle[i][j]);
+      dfs(i + 1, j + 1, sum + triangle[i][j]);
+      };
+    dfs(0, 0, 0);
+    return res;
+  }
+};
+}
