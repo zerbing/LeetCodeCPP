c76b8a22016490c224a069b9cff43ec158e2dff0
wuwl <wuwl@mail.jj.cn>
2024-02-02 15:05:33 +0800

    64. 最小路径和

--- dp.h
+++ dp.h
@@ -59,6 +59,58 @@ public:
 };
 }
 
+// 55. 跳跃游戏
+namespace N55
+{
+//给你一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。
+//
+//判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。
+class Solution {
+public:
+  bool canJump(vector<int>& nums) {
+    // dp[i]表示到达i位置时剩余的最大步数
+    vector<int> dp(nums.size(), 0);
+    for (int i = 1; i < nums.size(); ++i)
+    {
+      // 要么使用上一步的剩余步数，要么使用当前位置的步数
+      dp[i] = max(dp[i - 1], nums[i - 1]) - 1;
+      if (dp[i] < 0)
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+}
+
+// 64. 最小路径和
+namespace N64
+{
+//给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+//
+//说明：每次只能向下或者向右移动一步。
+class Solution {
+public:
+  int minPathSum(vector<vector<int>>& grid) {
+    vector<vector<int>> dp(grid.size() + 1, vector<int>(grid[0].size() + 1, INT_MAX));
+    dp[1][1] = grid[0][0];
+    for (int i = 1; i <= grid.size(); ++i)
+    {
+      for (int j = 1; j <= grid[0].size(); ++j)
+      {
+        if (i == 1 && j == 1)
+        {
+          continue;
+        }
+        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
+      }
+    }
+    return dp[grid.size()][grid[0].size()];
+  }
+};
+}
+
 // 2645. 构造有效字符串的最少插入数
 namespace N2645
 {
