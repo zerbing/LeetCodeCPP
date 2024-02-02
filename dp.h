7665a341977dd9a1ad8de18abd9d8886e7f76824
wuwl <wuwl@mail.jj.cn>
2024-02-02 14:41:31 +0800

    32. 最长有效括号

--- dp.h
+++ dp.h
@@ -1,5 +1,37 @@
 #pragma once
 
+// 32. 最长有效括号
+namespace N32
+{
+// 给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+class Solution {
+public:
+  int longestValidParentheses(string s) {
+    int n = static_cast<int>(s.size());
+    // dp[i] 表示以 s[i] **结尾**的最长有效括号的长度
+    // 注意dp[n-1]并不是最终结果，dp[i]的最大值才是最终结果
+    vector<int> dp(n, 0);
+    int maxLen = 0;
+    for (int i = 1; i < n; ++i)
+    {
+      if (s[i] == ')')
+      {
+        if (s[i - 1] == '(')
+        {
+          dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+        }
+        else if (i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] == '(')
+        {
+          dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+        }
+        maxLen = max(maxLen, dp[i]);
+      }
+    }
+    return maxLen;
+  }
+};
+}
+
 // 2645. 构造有效字符串的最少插入数
 namespace N2645
 {
