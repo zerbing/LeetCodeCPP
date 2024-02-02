7903ac1d91138177e5e470384ce3c0c673397a44
wuwl <wuwl@mail.jj.cn>
2024-02-02 18:20:15 +0800

    174. 地下城游戏 (困难)

--- dp.h
+++ dp.h
@@ -268,7 +268,7 @@ namespace N152
 class Solution {
 public:
   int maxProduct(vector<int>& nums) {
-    // dp[i][0]表示以nums[i]结尾的最大乘积，dp[i][1]表示以nums[i]结尾的最小乘积
+    // dp[i].first表示以nums[i]结尾的最大乘积，dp[i].second表示以nums[i]结尾的最小乘积
     vector<pair<int, int>> dp(nums.size(), {INT_MIN, INT_MAX});
     dp[0] = {nums[0], nums[0]};
     int maxProduct = nums[0];
@@ -279,6 +279,39 @@ public:
       maxProduct = max(maxProduct, dp[i].first);
     }
     return maxProduct;
+  }
+};
+}
+
+// 174. 地下城游戏 (困难)
+namespace N174
+{
+//恶魔们抓住了公主并将她关在了地下城 dungeon 的 右下角 。地下城是由 m x n 个房间组成的二维网格。我们英勇的骑士最初被安置在 左上角 的房间里，他必须穿过地下城并通过对抗恶魔来拯救公主。
+//
+//骑士的初始健康点数为一个正整数。如果他的健康点数在某一时刻降至 0 或以下，他会立即死亡。
+//
+//有些房间由恶魔守卫，因此骑士在进入这些房间时会失去健康点数（若房间里的值为负整数，则表示骑士将损失健康点数）；其他房间要么是空的（房间里的值为 0），要么包含增加骑士健康点数的魔法球（若房间里的值为正整数，则表示骑士将增加健康点数）。
+//
+//为了尽快解救公主，骑士决定每次只 向右 或 向下 移动一步。
+//
+//返回确保骑士能够拯救到公主所需的最低初始健康点数。
+//
+//注意：任何房间都可能对骑士的健康点数造成威胁，也可能增加骑士的健康点数，包括骑士进入的左上角房间以及公主被监禁的右下角房间。
+class Solution {
+public:
+  int calculateMinimumHP(vector<vector<int>>& dungeon) {
+    // dp[i][j]表示进入dungeon[i][j]时的最小初始健康点数
+    vector<vector<int>> dp(dungeon.size() + 1, vector<int>(dungeon[0].size() + 1, INT_MAX));
+    dp[dungeon.size()][dungeon[0].size() - 1] = 1;
+    dp[dungeon.size() - 1][dungeon[0].size()] = 1;
+    for (int i = dungeon.size() - 1; i >= 0; --i)
+    {
+      for (int j = dungeon[0].size() - 1; j >= 0; --j)
+      {
+        dp[i][j] = max(1, min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j]);
+      }
+    }
+    return dp[0][0];
   }
 };
 }
