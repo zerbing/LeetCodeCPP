c1dd3acdfbca90a4d26bcb2989a4b0750e0155d1
wuwl <wuwl@mail.jj.cn>
2024-02-02 14:47:24 +0800

    45. 跳跃游戏 II

--- dp.h
+++ dp.h
@@ -32,6 +32,33 @@ public:
 };
 }
 
+// 45. 跳跃游戏 II
+namespace N45
+{
+//给定一个长度为 n 的 0 索引整数数组 nums。初始位置为 nums[0]。
+//
+//每个元素 nums[i] 表示从索引 i 向前跳转的最大长度。换句话说，如果你在 nums[i] 处，你可以跳转到任意 nums[i + j] 处:
+//
+//0 <= j <= nums[i]
+//i + j < n
+//  返回到达 nums[n - 1] 的最小跳跃次数。生成的测试用例可以到达 nums[n - 1]。
+class Solution {
+public:
+  int jump(vector<int>& nums) {
+    vector<int> dp(nums.size(), INT_MAX);
+    dp[0] = 0;
+    for (int i = 0; i < nums.size(); ++i)
+    {
+      for (int j = 1; j <= nums[i] && i + j < nums.size(); ++j)
+      {
+        dp[i + j] = min(dp[i + j], dp[i] + 1);
+      }
+    }
+    return dp[nums.size() - 1];
+  }
+};
+}
+
 // 2645. 构造有效字符串的最少插入数
 namespace N2645
 {
