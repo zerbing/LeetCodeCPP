1f9582548d37be669ccd0e27145464bcddaa8318
wuwl <wuwl@mail.jj.cn>
2024-02-04 14:39:25 +0800

    343. 整数拆分 (中等)

--- dp.h
+++ dp.h
@@ -479,7 +479,24 @@ public:
 // 343. 整数拆分 (中等)
 namespace N343
 {
-  
+//给定一个正整数 n ，将其拆分为 k 个 正整数 的和（ k >= 2 ），并使这些整数的乘积最大化。
+//
+//返回 你可以获得的最大乘积 。
+class Solution {
+public:
+  int integerBreak(int n) {
+    vector<int> dp(n + 1, 0);
+    dp[1] = 1;
+    for (int i = 2; i <= n; ++i)
+    {
+      for (int j = 1; j < i; ++j)
+      {
+        dp[i] = max({ dp[i], j * (i - j), j * dp[i - j] });
+      }
+    }
+    return dp[n];
+  }
+};
 }
 
 // 2645. 构造有效字符串的最少插入数 (中等)
