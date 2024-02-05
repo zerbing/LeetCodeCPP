ad810aaaf1e5f9422fe03551b64450b498f845d0
wuwl <wuwl@mail.jj.cn>
2024-02-05 15:27:05 +0800

    2706. 购买两块巧克力 (简单)

--- greedy.h
+++ greedy.h
@@ -105,3 +105,21 @@ public:
   }
 };
 }
+
+// 1402. 做菜顺序 (困难)
+namespace N1402
+{
+//一个厨师收集了他 n 道菜的满意程度 satisfaction ，这个厨师做出每道菜的时间都是 1 单位时间。
+//
+//一道菜的 「 like - time 系数 」定义为烹饪这道菜结束的时间（包含之前每道菜所花费的时间）乘以这道菜的满意程度，也就是 time[i] * satisfaction[i] 。
+//
+//返回厨师在准备了一定数量的菜肴后可以获得的最大 like - time 系数 总和。
+//
+//你可以按 任意 顺序安排做菜的顺序，你也可以选择放弃做某些菜来获得更大的总和。
+class Solution {
+public:
+  int maxSatisfaction(vector<int>& satisfaction) {
+
+  }
+};
+}
