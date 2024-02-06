493b58b284cef4091b8df3a9b2bc154e760e3dff
wuwl <wuwl@mail.jj.cn>
2024-02-06 16:55:14 +0800

    24. 两两交换链表中的节点 (中等)

--- list_.h
+++ list_.h
@@ -76,6 +76,40 @@ public:
 };
 }
 
+// 24. 两两交换链表中的节点 (中等)
+namespace n24
+{
+// 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+  ListNode* swapPairs(ListNode* head) {
+    ListNode* dummy = new ListNode(0);
+    ListNode* cur = dummy;
+    cur->next = head;
+    while (cur->next != nullptr && cur->next->next != nullptr)
+    {
+      ListNode* node1 = cur->next;
+      ListNode* node2 = cur->next->next;
+      cur->next = node2;
+      node1->next = node2->next;
+      node2->next = node1;
+      cur = node1;
+    }
+    ListNode* res = dummy->next;
+    delete dummy;
+    return res;
+  }
+};
+}
+
 // LCR 077. 排序链表 (中等)
 namespace lcr077
 {
