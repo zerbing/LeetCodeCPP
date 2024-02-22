6c4aa1a0bd9e4a46bf131e74968c3ade3dd30222
wuwl <wuwl@mail.jj.cn>
2024-02-22 11:01:57 +0800

    30. 串联所有单词的子串

--- string_.h
+++ string_.h
@@ -1,5 +1,69 @@
 #pragma once
 
+// 30. 串联所有单词的子串
+namespace n30
+{
+//给定一个字符串 s 和一个字符串数组 words。 words 中所有字符串 长度相同。
+//
+//s 中的 串联子串 是指一个包含  words 中所有字符串以任意顺序排列连接起来的子串。
+//
+//例如，如果 words = ["ab", "cd", "ef"]， 那么 "abcdef"， "abefcd"，"cdabef"， "cdefab"，"efabcd"， 和 "efcdab" 都是串联子串。 "acdbef" 不是串联子串，因为他不是任何 words 排列的连接。
+//返回所有串联子串在 s 中的开始索引。你可以以 任意顺序 返回答案。
+class Solution {
+public:
+  vector<int> findSubstring(string s, vector<string>& words) {
+    vector<int> res;
+    if (s.empty() || words.empty())
+    {
+      return res;
+    }
+    int wordLen = static_cast<int>(words[0].size());
+    int wordCount = static_cast<int>(words.size());
+    int totalLen = wordLen * wordCount;
+    unordered_map<string, int> wordMap;
+    for (auto& word : words)
+    {
+      wordMap[word]++;
+    }
+    for (int i = 0; i < wordLen; ++i)
+    {
+      int left = i;
+      int right = i;
+      int count = 0;
+      unordered_map<string, int> window;
+      while (right + wordLen <= s.size())
+      {
+        string word = s.substr(right, wordLen);
+        right += wordLen;
+        if (wordMap.count(word))
+        {
+          window[word]++;
+          count++;
+          while (window[word] > wordMap[word])
+          {
+            string leftWord = s.substr(left, wordLen);
+            left += wordLen;
+            window[leftWord]--;
+            count--;
+          }
+          if (count == wordCount)
+          {
+            res.push_back(left);
+          }
+        }
+        else
+        {
+          left = right;
+          count = 0;
+          window.clear();
+        }
+      }
+    }
+    return res;
+  }
+};
+}
+
 // 383. 赎金信 (简单)
 namespace n383
 {
