dbd2c1b804ff464dc954d46f6ae675d8c6a84134
wuwl <wuwl@mail.jj.cn>
2024-02-22 14:44:29 +0800

    35. 搜索插入位置 (简单)

--- math_.h
+++ math_.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
 
-// 29. 两数相除
+// 29. 两数相除 (中等）
 namespace n29
 {
 //给你两个整数，被除数 dividend 和除数 divisor。将两数相除，要求 不使用 乘法、除法和取余运算。
--- vector_.h
+++ vector_.h
@@ -204,6 +204,37 @@ public:
 };
 }
 
+// 35. 搜索插入位置 (简单)
+namespace n35
+{
+//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+//
+//请必须使用时间复杂度为 O(log n) 的算法。
+class Solution {
+public:
+  int searchInsert(vector<int>& nums, int target) {
+    int left = 0, right = nums.size();
+    while (left < right)
+    {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] == target)
+      {
+        right = mid;
+      }
+      else if (nums[mid] < target)
+      {
+        left = mid + 1;
+      }
+      else if (nums[mid] > target)
+      {
+        right = mid;
+      }
+    }
+    return left;
+  }
+};
+}
+
 // 283. 移动零
 namespace n283
 {
