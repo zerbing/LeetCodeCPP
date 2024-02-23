d02b49cf7ff5ccca6168cb8bc16b853e300decbb
wuwl <wuwl@mail.jj.cn>
2024-02-23 16:39:41 +0800

    88. 合并两个有序数组 (简单)

--- vector_.h
+++ vector_.h
@@ -424,6 +424,43 @@ public:
 };
 }
 
+// 88. 合并两个有序数组 (简单)
+namespace n88
+{
+//给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+//
+//请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+//
+//注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+class Solution {
+public:
+  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i = 0, j = 0;
+    std::vector<int> tmpNums1 = nums1;
+    int curIndex = 0;
+    while (i < m && j < n)
+    {
+      if (tmpNums1[i] <= nums2[j])
+      {
+        nums1[curIndex++] = tmpNums1[i++];
+      }
+      else
+      {
+        nums1[curIndex++] = nums2[j++];
+      }
+    }
+    while (i < m)
+    {
+      nums1[curIndex++] = tmpNums1[i++];
+    }
+    while (j < n)
+    {
+      nums1[curIndex++] = nums2[j++];
+    }
+  }
+};
+}
+
 // 283. 移动零 (简单)
 namespace n283
 {
