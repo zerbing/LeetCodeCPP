d35fed69956c5c38018bbe9177d91bdf999a4909
wuwl <wuwl@mail.jj.cn>
2024-02-23 13:59:43 +0800

    69. x 的平方根

--- math_.h
+++ math_.h
@@ -127,6 +127,40 @@ public:
 };
 }
 
+// 69. x 的平方根
+namespace n69
+{
+//给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+//
+//由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+//
+//注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x** 0.5 。
+class Solution {
+public:
+  int mySqrt(int x) {
+    long long left = 0, right = x + 1;
+    while (left < right)
+    {
+      long long mid = left + (right - left) / 2;
+      long long mid2 = mid * mid;
+      if (mid2 == x)
+      {
+        return mid;
+      }
+      else if (mid2 < x)
+      {
+        left = mid + 1;
+      }
+      else if (mid2 > x)
+      {
+        right = mid;
+      }
+    }
+    return left - 1;
+  }
+};
+}
+
 // 2591. 将钱分给最多的儿童 (简单）
 namespace n2591
 {
