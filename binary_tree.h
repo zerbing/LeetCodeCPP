337e1d5ab342b377d33239cff7247600ac7ea38e
wuwl <wuwl@mail.jj.cn>
2024-02-29 19:43:26 +0800

    2581. 统计可能的树根数目 (困难)

--- binary_tree.h
+++ binary_tree.h
@@ -1181,6 +1181,27 @@ public:
 };
 }
 
+// 2581. 统计可能的树根数目 (困难)
+namespace n2581
+{
+//Alice 有一棵 n 个节点的树，节点编号为 0 到 n - 1 。树用一个长度为 n - 1 的二维整数数组 edges 表示，其中 edges[i] = [ai, bi] ，表示树中节点 ai 和 bi 之间有一条边。
+//
+//Alice 想要 Bob 找到这棵树的根。她允许 Bob 对这棵树进行若干次 猜测 。每一次猜测，Bob 做如下事情：
+//
+//选择两个 不相等 的整数 u 和 v ，且树中必须存在边[u, v] 。
+//Bob 猜测树中 u 是 v 的 父节点 。
+//Bob 的猜测用二维整数数组 guesses 表示，其中 guesses[j] = [uj, vj] 表示 Bob 猜 uj 是 vj 的父节点。
+//
+//Alice 非常懒，她不想逐个回答 Bob 的猜测，只告诉 Bob 这些猜测里面 至少 有 k 个猜测的结果为 true 。
+//
+//给你二维整数数组 edges ，Bob 的所有猜测和整数 k ，请你返回可能成为树根的 节点数目 。如果没有这样的树，则返回 0。
+class Solution {
+public:
+  int rootCount([[maybe_unused]] vector<vector<int>>& edges, [[maybe_unused]] vector<vector<int>>& guesses, int k) {
+  }
+};
+}
+
 // 2641. 二叉树的堂兄弟节点 II (中等)
 namespace n2641
 {
