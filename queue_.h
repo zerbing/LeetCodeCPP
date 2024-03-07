527e0dc0390e6e899ee6d4a7f4b803111c9c16db
wuwl <wuwl@mail.jj.cn>
2024-03-07 11:43:22 +0800

    225. 用队列实现栈 (简单)

--- queue_.h
+++ queue_.h
@@ -140,3 +140,70 @@ public:
   }
 };
 }
+
+// 225. 用队列实现栈 (简单)
+namespace n225
+{
+//请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+//
+//实现 MyStack 类：
+//
+//void push(int x) 将元素 x 压入栈顶。
+//int pop() 移除并返回栈顶元素。
+//int top() 返回栈顶元素。
+//boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
+class MyStack {
+public:
+  /** Initialize your data structure here. */
+  MyStack() {
+  }
+
+  /** Push element x onto stack. */
+  void push(int x) {
+    q1.push(x);
+  }
+
+  /** Removes the element on top of the stack and returns that element. */
+  int pop() {
+    while (q1.size() > 1)
+    {
+      q2.push(q1.front());
+      q1.pop();
+    }
+    int res = q1.front();
+    q1.pop();
+    swap(q1, q2);
+    return res;
+  }
+
+  /** Get the top element. */
+  int top() {
+    while (q1.size() > 1)
+    {
+      q2.push(q1.front());
+      q1.pop();
+    }
+    int res = q1.front();
+    q2.push(res);
+    q1.pop();
+    swap(q1, q2);
+    return res;
+  }
+
+  /** Returns whether the stack is empty. */
+  bool empty() {
+    return q1.empty();
+  }
+private:
+  queue<int> q1;
+  queue<int> q2;
+};
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+}
