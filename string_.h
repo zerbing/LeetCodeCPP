c5d3e8830ea2c8f01d436d2b974c0fd83fd46233
wuwl <wuwl@mail.jj.cn>
2024-03-07 11:33:26 +0800

    2575. 找出字符串的可整除数组 (中等)

--- string_.h
+++ string_.h
@@ -266,6 +266,33 @@ class Solution {
 };
 }
 
+// 2575. 找出字符串的可整除数组 (中等)
+namespace n2575
+{
+//给你一个下标从 0 开始的字符串 word ，长度为 n ，由从 0 到 9 的数字组成。另给你一个正整数 m 。
+//
+//word 的 可整除数组 div  是一个长度为 n 的整数数组，并满足：
+//
+//如果 word[0, ..., i] 所表示的 数值 能被 m 整除，div[i] = 1
+//否则，div[i] = 0
+//返回 word 的可整除数组。
+class Solution {
+public:
+  vector<int> divisibilityArray(string word, int m) {
+    vector<int> res;
+    long long left = 0;
+    for (auto c : word)
+    {
+      left *= 10;
+      left += c - '0';
+      left %= m;
+      res.push_back((left == 0) ? 1 : 0);
+    }
+    return res;
+  }
+};
+}
+
 // 2678. 老人的数目 (简单)
 namespace n2678
 {
