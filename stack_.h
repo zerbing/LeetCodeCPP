c23322efb3a5f3639c43b47e782c2810c0374528
wuwl <wuwl@mail.jj.cn>
2024-03-08 11:47:17 +0800

    232. 用栈实现队列

--- stack_.h
+++ stack_.h
@@ -46,6 +46,81 @@ public:
 };
 }
 
+// 232. 用栈实现队列
+namespace n232
+{
+//请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
+//
+//实现 MyQueue 类：
+//
+//void push(int x) 将元素 x 推到队列的末尾
+//int pop() 从队列的开头移除并返回元素
+//int peek() 返回队列开头的元素
+//boolean empty() 如果队列为空，返回 true ；否则，返回 false
+//说明：
+//
+//你 只能 使用标准的栈操作 —— 也就是只有 push to top, peek / pop from top, size, 和 is empty 操作是合法的。
+//你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+class MyQueue {
+public:
+  MyQueue() {
+  }
+
+  void push(int x) {
+    st1.push(x);
+  }
+
+  int pop() {
+    while (st1.size() > 1)
+    {
+      st2.push(st1.top());
+      st1.pop();
+    }
+    int res = st1.top();
+    st1.pop();
+    while (st2.size() > 0)
+    {
+      st1.push(st2.top());
+      st2.pop();
+    }
+    return res;
+  }
+
+  int peek() {
+    while (st1.size() > 1)
+    {
+      st2.push(st1.top());
+      st1.pop();
+    }
+    int res = st1.top();
+    st1.pop();
+    st2.push(res);
+    while (st2.size() > 0)
+    {
+      st1.push(st2.top());
+      st2.pop();
+    }
+    return res;
+  }
+
+  bool empty() {
+    return st1.empty();
+  }
+private:
+  stack<int> st1;
+  stack<int> st2;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+}
+
 // 316. 去除重复字母 (中等)
 namespace n316
 {
