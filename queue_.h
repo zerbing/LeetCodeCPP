f239cc4ad81a4a5110df91a16184d153124e6531
wuwl <wuwl@mail.jj.cn>
2024-03-20 11:01:47 +0800

    239. 滑动窗口最大值 (困难)

--- queue_.h
+++ queue_.h
@@ -217,6 +217,25 @@ namespace n239
 class Solution {
 public:
   vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    vector<int> res;
+    deque<int> q;
+    for (size_t i = 0; i < nums.size(); ++i)
+    {
+      while (!q.empty() && nums[i] > nums[q.back()])
+      {
+        q.pop_back();
+      }
+      q.push_back(i);
+      if (i - q.front() >= k)
+      {
+        q.pop_front();
+      }
+      if (i >= k - 1)
+      {
+        res.push_back(nums[q.front()]);
+      }
+    }
+    return res;
   }
 };
 }
